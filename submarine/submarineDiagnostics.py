b6eb20e8fbf892f1552433de4daa700b7085f71d
jharding86 <james@techintrigue.com>
2021-12-18 06:52:03 -0700

    +Solved Day 3 Part 2 +corrected a bug that assumed a 12 bit integer had a bit representing 4096... whoops

--- submarine/submarineDiagnostics.py
+++ submarine/submarineDiagnostics.py
@@ -3,7 +3,7 @@ class submarineDiagnostics:
         self.diagMask = []
 
         x = 1
-        while(x <= 4096):
+        while(x <= 2048):
             self.diagMask.append(x)
             x = x*2
         
@@ -12,16 +12,21 @@ class submarineDiagnostics:
         print("Diagnostics Mask:")
         print(self.diagMask)
 
-        self.gammaRate          = 0
-        self.epsilonRate        = 0
-        self.powerConsumption   = 0
-        self.rawDiagnostics     = []
+        self.gammaRate              = 0
+        self.epsilonRate            = 0
+        self.powerConsumption       = 0
+        self.oxygenGeneratorRating  = 0
+        self.co2ScrubberRating      = 0
+        self.rawDiagnostics         = []
 
     def readDiagnostics(self):
         self.__read_diagnostic_file()
         self.__calculateGammaRate()
         self.__calculateEpsilonRate()
         self.__calculatePowerConsumption()
+        self.__calculateOxygenGeneratorRating()
+        self.__calculateCO2ScrubberRating()
+        self.__calculateLifeSupportRating()
 
     def __read_diagnostic_file(self):
         with open('inputFiles/day3_input.txt') as f:
@@ -34,10 +39,7 @@ class submarineDiagnostics:
     def __calculateGammaRate(self):
         gammaString = ""
         for i in range(len(self.diagMask)):
-            count = 0
-            for j in range(len(self.rawDiagnostics)):
-                if (self.rawDiagnostics[j] & self.diagMask[i]) == self.diagMask[i]:
-                    count += 1
+            count = self.__countBitsMatchingMaskInList(self.diagMask[i], self.rawDiagnostics)
             if count > len(self.rawDiagnostics)/2:
                 gammaString += "1"
             else:
@@ -45,6 +47,13 @@ class submarineDiagnostics:
 
         self.gammaRate = int(gammaString, 2)
         print("Gamma Rate: ", self.gammaRate, bin(self.gammaRate))
+
+    def __countBitsMatchingMaskInList(self, mask, list):
+        count = 0
+        for item in list:
+            if (item & mask) == mask:
+                count += 1
+        return count
 
     def __calculateEpsilonRate(self):
         self.epsilonRate = self.__bit_not(self.gammaRate)
@@ -56,3 +65,49 @@ class submarineDiagnostics:
 
     def __bit_not(self, n):
         return (1 << n.bit_length()) - 1 - n
+
+    def __popularBitCounter(self, onesOrZeros):
+        rawCopy = self.rawDiagnostics.copy()
+
+        for i in range(len(self.diagMask)):
+            if len(rawCopy) == 1:
+                break
+            # Determine the current bit criteria (which is more common, 1's or 0's)
+            count = self.__countBitsMatchingMaskInList(self.diagMask[i], rawCopy)
+            if count >= len(rawCopy)/2:#more 1's than 0's
+                #keep the on bits (1's)
+                if onesOrZeros:
+                    maskEquate = self.diagMask[i]
+                else:
+                    maskEquate = 0    
+            else:
+                #keep the off bits (0's)
+                if onesOrZeros:
+                    maskEquate = 0
+                else:
+                    maskEquate = self.diagMask[i]
+
+            # loop rawCopy removing items that don't match the current
+            # bit criteria that was determined above.
+            j = 0
+            rawLength = len(rawCopy)
+            while j < rawLength:
+                if rawCopy[j] & self.diagMask[i] != maskEquate:
+                    rawCopy.remove(rawCopy[j])
+                    rawLength = len(rawCopy)
+                else:
+                    j += 1
+
+        return rawCopy[0]
+        
+    def __calculateOxygenGeneratorRating(self):
+        self.oxygenGeneratorRating = self.__popularBitCounter(1)
+        print("Oxygen Generator Rating:", self.oxygenGeneratorRating)
+
+    def __calculateCO2ScrubberRating(self):
+        self.co2ScrubberRating = self.__popularBitCounter(0)
+        print("CO2 Scrubber Rating:", self.co2ScrubberRating)
+
+    def __calculateLifeSupportRating(self):
+        self.lifeSupportRating = self.co2ScrubberRating * self.oxygenGeneratorRating
+        print("Life Support Rating:", self.lifeSupportRating)
